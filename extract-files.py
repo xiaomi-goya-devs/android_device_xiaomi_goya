93e2266400b812f50a231481e501ddab85264a5e
Mashopy <eliasgheeraert@gmail.com>
2026-04-13 10:03:22 +0200

    more abi but google time

--- extract-files.py
+++ extract-files.py
@@ -82,7 +82,11 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsink.so', 'libsink-mtk.so'),
     (
         'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl',
-        'odm/lib64/hw/displayfeature.default.so',
+        'odm/lib64/hw/displayfeature.default.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    (
         'odm/lib64/libadaptivehdr.so',
         'odm/lib64/libcolortempmode.so',
         'odm/lib64/libdither.so',
@@ -101,6 +105,24 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib64/mt6899/libpqconfig.so'
     ): blob_fixup()
         .replace_needed('android.hardware.sensors-V2-ndk.so', 'android.hardware.sensors-V3-ndk.so'),
+    (
+        'odm/lib64/libmiXmlParser.so',
+        'vendor/lib64/hw/audio.primary.mediatek.so',
+        'vendor/lib64/hw/mt6899/vendor.mediatek.hardware.pq_aidl-impl.so',
+        'vendor/lib64/libHardwareBacklightcore.so',
+        'vendor/lib64/lib_power_applist.so',
+        'vendor/lib64/libaudiocloudctrl.so',
+        'vendor/lib64/libmicamera_aidl_provider.so',
+        'vendor/lib64/libmicamera_hal_core.so',
+        'vendor/lib64/libpowerhal.so',
+        'vendor/lib64/libpqxmlflagparser.so',
+        'vendor/lib64/libpqxmlparser.so',
+        'vendor/lib64/libsilkybrightnesscore.so',
+        'vendor/lib64/libxlog.so',
+        'vendor/lib64/mt6899/lib3a.custom.ae.flow.so',
+        'vendor/lib64/mt6899/libmmlpqImpl.so'
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'vendor/bin/hw/android.hardware.audio.service-aidl.mediatek': blob_fixup()
         .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
         .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
@@ -125,9 +147,13 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('android.hardware.graphics.common-V5-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     'vendor/lib64/hw/android.hardware.audio.effect.aidl-impl-mediatek.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so'),
+        .replace_needed('android.media.audio.common.types-V5-ndk.so', 'android.media.audio.common.types-V3-ndk.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'vendor/lib64/hw/android.hardware.soundtrigger3-impl.so': blob_fixup()
         .replace_needed('libaudio_aidl_conversion_common_ndk.so', 'libaudio_aidl_conversion_common_ndk_prebuilt.so'),
+    'vendor/lib64/hw/hwcomposer.mtk_common.so': blob_fixup()
+        .add_needed('libprocessgroup_shim.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     (
         'vendor/lib64/hw/hwcomposer.mtk_common.so',
         'vendor/lib64/mt6899/libmtkcam_taskmgr.so',
